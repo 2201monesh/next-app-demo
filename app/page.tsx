b083ef12b34e4ad103af15cd27e04ffc4a8224e2
Monesh Goyal <goyalmonesh2201@gmail.com>
2023-12-09 19:53:25 +0530

    Learned routing and use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <div>
       <h1>Hello World I am batman</h1>
       <Link href='/users'>User Page</Link>
+      <ProductCard />
     </div>
   )
 }
